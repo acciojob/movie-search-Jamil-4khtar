5fc04dd1d3434a71c5956394df09456cd339c348
Jamil-4khtar <jamil4khtar@gmail.com>
2024-12-31 18:23:11 +0530

    Add SearchMovies component and integrate it into App; update package.json dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import SearchMovies from "./SearchMovies";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <SearchMovies/>
     </div>
   )
 }
---
+++ src/components/SearchMovies.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+
+function SearchMovies() {
+    const [movies, setMovies] = useState([])
+    const [name, setName] = useState("")
+    const [error, setError] = useState("")
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch(`http://www.omdbapi.com/?apikey=99eb9fd1&s=${name}`)
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.Response == "False") {
+                    throw new Error(data.Error)
+                } else {
+                    setMovies(data.Search)
+                    setError("")
+                    console.log(data)
+                }
+            })
+            .catch((err) => {
+                setError(err.message)
+                setMovies([])
+                console.log(err)
+            })
+    }
+
+    console.log(movies)
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h1>Search Movie</h1>
+                <input
+                    type='text'
+                    placeholder='Enter an movie name'
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+                <button type='submit'>Search</button>
+            </form>
+
+
+
+            <div>
+                {error && <p>{error}</p>}
+                {
+                    movies && movies.map((item, index) => (
+                        <div key={index}>
+                            <img src={item.Poster} alt={item.Title} />
+                            <h2>{item.Title}</h2>
+                            <p>Type: {item.Type}</p>
+                            <p>Year: {item.Year}</p>
+                            <p>IMDB ID: {item.imdbID}</p>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default SearchMovies
